a13860046f6b2db0ff5a87a7d3dac142d33c554c
Ian Ro <ianrogers2422@gmail.com>
2021-11-10 22:01:55 -0500

    Text change added

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+    let intTemp = document.querySelector('.deg');
+    let intZone = document.querySelector('.location-timezone');
+    let intDescr = document.querySelector('.tempDescription');
+    let disVoice = document.querySelector('.disemVoice');
+    let globSearch = document.querySelector('.gSearch');
 
     const api = "https://api.openweathermap.org/data/2.5/weather?q=fulton,IL,US&appid=";
+    const eventAPI = "api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}";
 
     fetch(api).then(response => {return response.json();
     })
@@ -9,10 +15,36 @@
         intTemp.textContent = Math.floor((data.main.temp -273.15) * 9/5 + 32);
         intDescr.textContent = weatherDescr.charAt(0).toUpperCase() + weatherDescr.slice(1).toLowerCase() + " at Frank's House";
     });
+      
+          function voiceChange1(){
+        globSearch.addEventListener('click', () => {
+            disVoice.textContent = "By Global Search, you mean Frank's House right?"
+            voiceChange2()
+        });
+    }     
+             function voiceChange2(){
+        globSearch.addEventListener('click', () => {
+            disVoice.textContent = "Whose house? Frank's House, am I right?"
+            voiceChange3()
+        });
+    }     
+    function voiceChange3(){
+        globSearch.addEventListener('click', () => {
+            disVoice.textContent = "C'mon! You're going to wake him up, then we'll never get out of here!"
+            voiceChange4()
+        });
+    }     
+    function voiceChange4(){
+        globSearch.addEventListener('click', () => {
+            disVoice.textContent = "Awww, here he comes!"
+            voiceChange5()
+        });
+    }     
+    function voiceChange5(){
+        globSearch.addEventListener('click', () => {
+            disVoice.textContent = ""
+        });
+    }     
 
 
-
-let intTemp = document.querySelector('.deg');
-let intZone = document.querySelector('.location-timezone');
-let intDescr = document.querySelector('.tempDescription');
-
+      voiceChange1();
